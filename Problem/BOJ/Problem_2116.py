fa98a7e7f2e3d7e2377c9f42d0e147cb2dfcdb6d
wonjw3638 <wonjw3638@gmail.com>
2022-07-23 21:48:52 +0900

    Solve BOJ 220723

--- Problem/BOJ/Problem_2116.py
+++ Problem/BOJ/Problem_2116.py
@@ -1,36 +1,34 @@
-dice = { 0 : 5, 1 : 3, 2 : 4, 3 : 1, 4 : 2, 5 : 0 } # 아랫면 선택시 와야하는 윗면 idx
+dice_num = { 0 : 5, 1 : 3, 2 : 4, 3 : 1, 4 : 2, 5 : 0 } # 아랫면 선택시 와야하는 윗면 idx
 N = int(input()) # 주사위 개수
-temp = 0
-first = input().split()
+dice_list = []  # 주사위 값 입력, 저장
 now_idx , nex_idx , value = 0, 0, 0 # 현재 idx, 다음 idx, 맞닿는 주사위 값
+max_sum = 0   # 현재 단계까지 합의 최대값
 
-def max_val(now_idx, nex_idx, dice_val):
-    now = dice_val[now_idx]  #now, nex idx 제외, 최댓값 구하기
+for _ in range(N):
+    dice_list.append(list(map(int, input().split())))
+
+def max_val(now_idx, nex_idx, dice_val):  # 주사위에서 가장 큰 면의 값 구하기
+
+    now = dice_val[now_idx]  # now, nex idx 제외, 최댓값 구하기
     nex = dice_val[nex_idx]
-    dice_val[now_idx] = 0
-    dice_val[nex_idx] = 0
+    dice_val[now_idx], dice_val[nex_idx] = 0, 0
     max_value = max(dice_val) 
-    dice_val[now_idx] = now
+
+    dice_val[now_idx] = now  # 최댓값 구한 후 제외한 now, nex idx 해당하는 값 복구
     dice_val[nex_idx] = nex 
     return max_value
 
-for i in range(1,7):
-    ans = 0
-    value = i
-    now_idx = first.index(value)
-    nex_idx = dice[int(now_idx)]
-    value = first[nex_idx]
-    ans += max_val(now_idx, nex_idx, first)
-    for num in range(N-1):
-        dices = input().split()
-        now_idx = dices.index(value)
-        nex_idx = dice[int(now_idx)]
-        value = dices[nex_idx]
-        ans += max_val(now_idx, nex_idx, dices)
-    if ans > temp:
-        ans = temp
+for first_value in range(1,7):  # 첫번째 주사위 아랫면 값 택1 ( 1~6)
+    ans = 0 # 모든 주사위 가장 큰 면의 합
+    value = first_value
 
-print(ans)
+    for dice in dice_list:  # 모든 주사위에 대한 최댓값 탐색
+        now_idx = dice.index(value)  # 현재 해당 면에 대한 주사위 idx 값 찾기
+        nex_idx = dice_num.get(now_idx)  # 해당 idx 값에 따른 다음 면의 idx값 찾기
+        ans += max_val(now_idx, nex_idx, dice) # 위, 아랫 면 값을 제외한 면 중에 최댓값 찾아 더하기
+        value = dice[nex_idx]  # 바뀌는 면의 값 찾아서 다음 주사위로 넘어감
 
+    if ans > max_sum: 
+        max_sum = ans  # 모든 주사위 탐색후 합한 값이 전보다 크면 값 덮어쓰기
 
-########## 아직 푸는 중 ########## 
+print(max_sum)
