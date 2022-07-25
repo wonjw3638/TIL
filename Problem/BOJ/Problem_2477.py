889ef4277cd47c9556472879e14771c705ee191c
wonjw3638 <wonjw3638@gmail.com>
2022-07-25 21:56:12 +0900

    Solve BOJ 220725

---
+++ Problem/BOJ/Problem_2477.py
@@ -0,0 +1,34 @@
+N = int(input()) # 참외 개수
+defense = { 1 : 4, 2 : 3, 3 : 1, 4 : 2 }
+area = { i : 0 for i in [1, 2, 3, 4]}
+
+field = []
+for _ in range(6):
+    field.append(list(map(int, input().split())))
+
+print(field)
+
+temp = field[0][0]
+except_area = []
+total_area = []
+
+for i in field:
+    area[i[0]] += 1
+    if i[0] == temp:
+        temp = defense[i[0]]
+        continue
+    else:
+        except_area.append(i[1])
+
+for i in range(1,5):
+    if area[i] == 1:
+        for f in field:
+            if f[0] == i:
+                total_area.append(f[1])
+
+print(f'except_area : {except_area}')
+print(f'total_area : {total_area}')
+
+print((N * (total_area[0] * total_area[1]) - (except_area[0] * except_area[1])))
+
+####아직 수정 중
