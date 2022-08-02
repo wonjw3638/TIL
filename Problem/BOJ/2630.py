a28be0d39eeb7a796087ac8a3e07a4a74eac4c91
wonjw3638 <wonjw3638@gmail.com>
2022-08-02 18:00:11 +0900

    Solve BOJ 220802

---
+++ Problem/BOJ/2630.py
@@ -0,0 +1,13 @@
+N = int(input())
+blue = 0
+white = 0
+
+confetti = []  #색종이
+
+for i in range(N):
+    color = list(map(int, input().split()))
+    confetti.append(color)
+
+print(confetti)
+
+# 아직 푸는 중
