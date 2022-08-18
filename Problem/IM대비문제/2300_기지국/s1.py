0e0dc93a1c51e5d5e5ced32f1f3e42b6d9715f1f
wonjw3638 <wonjw3638@gmail.com>
2022-08-19 01:07:29 +0900

    Solve 220818 IM Problem

--- Problem/IM대비문제/2300_기지국/s1.py
+++ Problem/IM대비문제/2300_기지국/s1.py
@@ -2,8 +2,7 @@
 # 220818
 '''
 설계 14:35 ~ 14:51
-구현 15:05 ~ 17:00
-시간초과 ~! 
+구현 15:05 ~ 17:00 + 1h
 '''
 
 import sys
@@ -20,23 +19,34 @@ for _ in range(N):
 # 기지국 x좌표 기준으로 정렬
 node_info.sort()
 
+# x_min, x_max, y_max  메모이제이션
+cal_list = []
 
 # 필요한 통신범위 계산
-def cal(pos_list):
-    n = len(pos_list)
-    x_min = min(pos_list[i][0] for i in range(n))
-    x_max = max(pos_list[i][0] for i in range(n))
-    y_max = max(abs(pos_list[i][1]) for i in range(n))
-    max_val = max((x_max - x_min),(2 * y_max))
-    return max_val
+def cal(a,b):
+    if a == b-1:
+        cal_list.append([node_info[a][0], node_info[a][0], abs(node_info[a][1])])
+        return ( 2 * cal_list[a][2])
+    else:
+        x_min = min(cal_list[a][0], cal_list[b-1][0])
+        x_max = max(cal_list[a][1], cal_list[b-1][1])
+        y_max = max(cal_list[a][2], cal_list[b-1][2])
+        cal_list[a] = [x_min, x_max, y_max]
+        return max((x_max - x_min),(2 * y_max))
 
-min_table = [[] for _ in range(N+1)]
-min_table[0] = abs(node_info[0][1] * 2)
+# 최적의 값을 저장할 list
+min_table = [0] * (N+1)
+
 # 기지국 설치개수
 for j in range(1, N+1):
+    # 큰 값으로 설정
     q = 1000001
-    for i in range(j-1):
-        q = min(q, min_table[i] + cal(node_info[i:j]))
+    for i in range(1, j+1):
+        calcul = cal(j-i,j)
+        if calcul > q: 
+            continue
+        else:
+            q = min(q, calcul + min_table[j-i])
     min_table[j] = q
 
-print(min_table[N-1])
+print(min_table[N])
