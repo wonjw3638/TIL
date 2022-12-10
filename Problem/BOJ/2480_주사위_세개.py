4246ab959011ba029fc92444883227fd8f533488
wonjw3638 <wonjw3638@gmail.com>
2022-12-11 01:34:19 +0900

    Solve BOJ

---
+++ Problem/BOJ/2480_주사위_세개.py
@@ -0,0 +1,24 @@
+# 2480 주사위 세개
+
+'''
+시작시각 03:30
+종료시각
+'''
+
+def game(num1, num2, num3):
+    if num1 == num2 and num2 == num3:
+        return 10000 + num1 * 1000
+    elif num1 == num2 and num2 != num3:
+        return 1000 + num1 * 100
+    elif num1 == num3 and num1 != num2:
+        return 1000 + num1 * 100
+    elif num2 == num3 and num1 != num2:
+        return 1000 + num2 * 100
+    else:
+        return max(num1, num2, num3) * 100
+
+
+num1, num2, num3 = map(int, input().split())
+answer = game(num1, num2, num3)
+
+print(answer)
